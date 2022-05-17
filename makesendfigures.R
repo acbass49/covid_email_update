bc2c6c8bc5913011de4b5c9d9c077ecbb0783b34
Alex Bass <acbass49@gmail.com>
2022-05-17 00:42:21 -0400

    testing github

--- makesendfigures.R
+++ makesendfigures.R
@@ -223,7 +223,6 @@ patchwork + plot_annotation(
 
 ggsave("national.png", units = "in", width = 6.92, height = 6.85)
 
-
 #determining if cases are increasing or decreasing in a state
 starting <- ymd(Sys.Date())-12
 ending <- ymd(Sys.Date())-5
